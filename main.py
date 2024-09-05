14e2fb4f46910c0347cf8d9137ef1e8b95a9c710
CuttrellW <wcuttrell@gmail.com>
2024-09-04 21:55:54 -0600

    add check for update

--- main.py
+++ main.py
@@ -2,9 +2,11 @@ import logging
 import flask
 import os
 import gspread
+import pandas as pd
 
 from google.cloud import storage
 from google.oauth2.service_account import Credentials
+from datetime import datetime as dt
 
 app = flask.Flask(__name__)
 logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
@@ -12,7 +14,7 @@ logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(
 
 def get_credentials():
     # Get credentials from service account key
-    scopes = ['https://www.googleapis.com/auth/spreadsheets', 'https://www.googleapis.com/auth/drive']
+    scopes = ['https://www.googleapis.com/auth/spreadsheets', 'https://www.googleapis.com/auth/drive', 'https://www.googleapis.com/auth/devstorage.read_write']
     return Credentials.from_service_account_file(os.environ["GCP_SERVICE_ACCOUNT_KEY"], scopes=scopes)
 
 
@@ -27,9 +29,46 @@ class GoogleSheetsImport:
         self.file_name = file_name
         self.sheet_id = sheet_id
 
+    def sheet_updated(self, spreadsheet: gspread.Spreadsheet) -> bool:
+        # Get Sheet last modified time
+        sheet_last_updated = dt.strptime(spreadsheet.lastUpdateTime, "%Y-%m-%dT%H:%M:%S.%fZ")
+        # Get GCS file last modified time
+        blob = self.bucket_client.blob(self.file_name)
+        if not blob.exists():
+            return True  # If file does not exist, return True to upload
+
+        # Reload blob to get the updated timestamp
+        blob.reload()
+        blob_last_updated = blob.updated
+
+        # Check Sheet was modified after file was last updated
+        logging.info(f"Sheet modified: {sheet_last_updated} Blob modified: {blob_last_updated}")
+        return sheet_last_updated > blob_last_updated if blob_last_updated else True
+
     def process_sheet(self):
-        # process the sheet
-        pass
+        # Open Google Sheet
+        logging.info(f"Processing {self.file_name}...")
+        spreadsheet = self.sheet_client.open_by_key(self.sheet_id)
+        if not self.sheet_updated(spreadsheet):
+            logging.info(f"No updates found for {self.file_name}\n")
+            return
+
+        # Create DataFrame from Google Sheet
+        df = pd.DataFrame(spreadsheet.sheet1.get_all_records())
+
+        # Save df as file to temporary location
+        temp_path = f"/tmp/{self.file_name}"
+        if self.file_name.endswith('.csv'):
+            df.to_csv(temp_path, index=False, header=True)
+        elif self.file_name.endswith('.json'):
+            df.to_json(temp_path, index=False)
+        else:
+            logging.error(f"Unsupported file format: {self.file_name}")
+            return
+
+        # Upload local file to GCS
+        self.bucket_client.blob(self.file_name).upload_from_filename(temp_path)
+        logging.info(f"Uploaded to GCS")
 
 
 @app.route('/process_sheet', methods=['POST'])
